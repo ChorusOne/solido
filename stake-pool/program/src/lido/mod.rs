85e69ce0ae3bab2b63b0ba15584c219cd46af2d6
E. Fynn <me@enriquefynn.com>
2021-04-13 20:53:43 +0200

    multisig dao for self-upgradable lido

--- stake-pool/program/src/lido/mod.rs
+++ stake-pool/program/src/lido/mod.rs
@@ -1,2 +1,1 @@
-pub mod entrypoint_lido;
 pub mod processor;
--- stake-pool/program/src/lido/processor.rs
+++ stake-pool/program/src/lido/processor.rs
@@ -1,5 +1,7 @@
 //! Program state processor
 
+use std::collections::HashSet;
+
 use {
     crate::{
         borsh::try_from_slice_unchecked,
@@ -36,34 +38,154 @@ use {
 #[repr(C)]
 #[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
 pub struct Lido {
-    stakepool: Pubkey,
-    members: Vec<Pubkey>,
+    stake_pool_account: Pubkey,
+    members_account: Pubkey,
+}
+
+#[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
+pub enum LidoAccountType {
+    Uninitialized,
+    Initialized,
+}
+
+impl Default for LidoAccountType {
+    fn default() -> Self {
+        LidoAccountType::Uninitialized
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
+pub struct LidoMembers {
+    /// Account type, must be LidoMembers currently
+    pub account_type: LidoAccountType,
+
+    maximum_members: u32,
+    list: Vec<Pubkey>,
+}
+
+impl LidoMembers {
+    pub fn new(maximum_members: u32) -> Self {
+        Self {
+            account_type: LidoAccountType::Uninitialized,
+            maximum_members: maximum_members,
+            list: vec![Pubkey::default(); maximum_members as usize],
+        }
+    }
+
+    pub fn is_initialized(&self) -> bool {
+        self.account_type != LidoAccountType::Uninitialized
+    }
 }
 
 #[repr(C)]
 #[derive(Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize, BorshSchema)]
 pub enum LidoInstruction {
     Initialize {
-        stake_pool: Pubkey,
-        member_list: Vec<Pubkey>,
+        stake_pool_account: Pubkey,
+        members_list_account: Vec<Pubkey>,
     },
     Deposit,
     Withdraw(u64),
+    Upgrade {
+        buffer_address: Pubkey,
+        spill_address: Pubkey,
+    },
 }
+
 /// Program state handler.
-pub struct Processor {}
+pub struct Processor;
 impl Processor {
     pub fn process_initialize(
         program_id: &Pubkey,
         accounts: &[AccountInfo],
-        stake_pool: Pubkey,
-        member_list: Vec<Pubkey>,
+        stake_pool_account: Pubkey,
+        new_members: &[Pubkey],
     ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let lido_info = next_account_info(account_info_iter)?;
+        let members_list_info = next_account_info(account_info_iter)?;
+        let rent_info = next_account_info(account_info_iter)?;
+
+        let rent = &Rent::from_account_info(rent_info)?;
+
+        let members_list =
+            try_from_slice_unchecked::<LidoMembers>(&members_list_info.data.borrow())?;
+
+        let mut lido = try_from_slice_unchecked::<Lido>(&lido_info.data.borrow())?;
+
+        if members_list.is_initialized() {
+            return Err(StakePoolError::AlreadyInUse.into());
+        }
+        if !rent.is_exempt(members_list_info.lamports(), members_list_info.data_len()) {
+            msg!("Members list is not rent-exempt");
+            return Err(ProgramError::AccountNotRentExempt);
+        }
+
+        if !rent.is_exempt(lido_info.lamports(), lido_info.data_len()) {
+            msg!("Lido is not rent-exempt");
+            return Err(ProgramError::AccountNotRentExempt);
+        }
+
+        let mut members_list = new_members
+            .into_iter()
+            .fold(members_list, |mut acc, member| {
+                acc.list.push(member.to_owned());
+                acc
+            });
+        members_list.account_type = LidoAccountType::Initialized;
+        members_list.serialize(&mut *members_list_info.data.borrow_mut())?;
+
+        lido.members_account = *members_list_info.key;
+        lido.serialize(&mut *lido_info.data.borrow_mut())?;
+
         Ok(())
     }
+
     pub fn process_deposit(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
         Ok(())
     }
+
+    pub fn process_upgrade(
+        program_id: &Pubkey,
+        buffer_address: &Pubkey,
+        spill_address: &Pubkey,
+        accounts: &[AccountInfo],
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let lido_info = next_account_info(account_info_iter)?;
+        let members_list_info = next_account_info(account_info_iter)?;
+
+        let signed_accounts = account_info_iter
+            .filter(|acc| acc.is_signer)
+            .map(|acc| acc.key.to_owned())
+            .collect::<HashSet<Pubkey>>();
+
+        let mut lido = try_from_slice_unchecked::<Lido>(&lido_info.data.borrow())?;
+        let members_list =
+            try_from_slice_unchecked::<LidoMembers>(&members_list_info.data.borrow())?;
+
+        // TODO: Check if lido is valid
+
+        let num_signatures = members_list
+            .list
+            .iter()
+            .filter(|member| signed_accounts.contains(member))
+            .count();
+
+        if num_signatures < members_list.list.len() {
+            return Err(StakePoolError::SignatureMissing.into());
+        }
+
+        let ix = solana_program::bpf_loader_upgradeable::upgrade(
+            program_id,
+            buffer_address,
+            program_id,
+            spill_address,
+        );
+        invoke(&ix, accounts)
+    }
+
     pub fn process_withdraw(
         program_id: &Pubkey,
         pool_tokens: u64,
@@ -77,13 +199,22 @@ impl Processor {
         let instruction = LidoInstruction::try_from_slice(input)?;
         match instruction {
             LidoInstruction::Initialize {
-                stake_pool,
-                member_list,
-            } => Self::process_initialize(program_id, accounts, stake_pool, member_list),
+                stake_pool_account,
+                members_list_account,
+            } => Self::process_initialize(
+                program_id,
+                accounts,
+                stake_pool_account,
+                &members_list_account,
+            ),
             LidoInstruction::Deposit {} => Self::process_deposit(program_id, accounts),
             LidoInstruction::Withdraw(amount) => {
                 Self::process_withdraw(program_id, amount, accounts)
             }
+            LidoInstruction::Upgrade {
+                buffer_address,
+                spill_address,
+            } => Self::process_upgrade(program_id, &buffer_address, &spill_address, accounts),
         }
     }
 }
