ec527c22414cf1e9e9fde71e8d1609590e3f1a61
E. Fynn <me@enriquefynn.com>
2021-04-22 19:34:58 +0400

    Deposit skel & test

---
+++ lido/program/src/instruction.rs
@@ -0,0 +1,104 @@
+use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    system_program, sysvar,
+};
+
+#[repr(C)]
+#[derive(Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize, BorshSchema)]
+pub enum LidoInstruction {
+    Initialize,
+    /// Deposit with amount
+    Deposit {
+        amount: u64,
+    },
+    /// Deposit amount to member validator
+    DelegateDeposit {
+        amount: u64,
+        member: Pubkey,
+    },
+    Withdraw {
+        amount: u64,
+    },
+}
+
+pub fn initialize(
+    program_id: &Pubkey,
+    lido: &Pubkey,
+    stake_pool: &Pubkey,
+    owner: &Pubkey,
+    mint_program: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    let init_data = LidoInstruction::Initialize;
+    let data = init_data.try_to_vec()?;
+    let accounts = vec![
+        AccountMeta::new(*lido, true),
+        AccountMeta::new_readonly(*stake_pool, true),
+        AccountMeta::new_readonly(*owner, false),
+        AccountMeta::new(*mint_program, false),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+// let account_info_iter = &mut accounts.iter();
+//         // Lido
+//         let lido_info = next_account_info(account_info_iter)?;
+//         // Stake pool
+//         let stake_pool = next_account_info(account_info_iter)?;
+//         // Owner program
+//         let owner_info = next_account_info(account_info_iter)?;
+//         // User account
+//         let user_info = next_account_info(account_info_iter)?;
+//         // Recipient account
+//         let lsol_recipient_info = next_account_info(account_info_iter)?;
+//         // Token minter
+//         let lsol_mint_info = next_account_info(account_info_iter)?;
+//         // Token program account (SPL Token Program)
+//         let token_program_info = next_account_info(account_info_iter)?;
+//         // Lido authority account
+//         let authority_info = next_account_info(account_info_iter)?;
+//         // Reserve account
+//         let reserve_account_info = next_account_info(account_info_iter)?;
+//         // System program
+//         let system_program_info = next_account_info(account_info_iter)?;
+
+pub fn deposit(
+    program_id: &Pubkey,
+    lido: &Pubkey,
+    stake_pool: &Pubkey,
+    owner: &Pubkey,
+    user: &Pubkey,
+    recipient: &Pubkey,
+    mint_program: &Pubkey,
+    authority: &Pubkey,
+    reserve_account: &Pubkey,
+    amount: u64,
+) -> Result<Instruction, ProgramError> {
+    let init_data = LidoInstruction::Deposit { amount: amount };
+    let data = init_data.try_to_vec()?;
+    let accounts = vec![
+        AccountMeta::new(*lido, false),
+        AccountMeta::new_readonly(*stake_pool, false),
+        AccountMeta::new_readonly(*owner, false),
+        AccountMeta::new(*user, true),
+        AccountMeta::new(*recipient, false),
+        AccountMeta::new(*mint_program, false),
+        AccountMeta::new_readonly(spl_token::id(), false),
+        AccountMeta::new_readonly(*authority, false),
+        AccountMeta::new(*reserve_account, false),
+        AccountMeta::new_readonly(system_program::id(), false),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
---
+++ lido/program/tests/deposit.rs
@@ -0,0 +1,76 @@
+mod helpers;
+
+use helpers::{
+    program_test,
+    stakepool_account::{create_token_account, get_account, get_token_balance, transfer},
+    LidoAccounts,
+};
+use lido::{id, instruction, state};
+use solana_program::{borsh::get_packed_len, hash::Hash};
+use solana_program_test::{tokio, BanksClient};
+use solana_sdk::{
+    signature::{Keypair, Signer},
+    transaction::Transaction,
+};
+
+async fn setup() -> (BanksClient, Keypair, Hash, LidoAccounts) {
+    let (mut banks_client, payer, recent_blockhash) = program_test().start().await;
+    let mut lido_accounts = LidoAccounts::new();
+    lido_accounts
+        .initialize_lido(&mut banks_client, &payer, &recent_blockhash)
+        .await
+        .unwrap();
+
+    (banks_client, payer, recent_blockhash, lido_accounts)
+}
+pub const TEST_DEPOSIT_AMOUNT: u64 = 1000;
+
+#[tokio::test]
+async fn test_deposit() {
+    let (mut banks_client, payer, recent_blockhash, lido_accounts) = setup().await;
+
+    let user = Keypair::new();
+    let recipient = Keypair::new();
+
+    create_token_account(
+        &mut banks_client,
+        &payer,
+        &recent_blockhash,
+        &recipient,
+        &lido_accounts.mint_program.pubkey(),
+        &user.pubkey(),
+    )
+    .await
+    .unwrap();
+
+    transfer(
+        &mut banks_client,
+        &payer,
+        &recent_blockhash,
+        &user.pubkey(),
+        TEST_DEPOSIT_AMOUNT,
+    )
+    .await;
+
+    let mut transaction = Transaction::new_with_payer(
+        &[instruction::deposit(
+            &id(),
+            &lido_accounts.lido.pubkey(),
+            &lido_accounts.stake_pool.pubkey(),
+            &lido_accounts.owner.pubkey(),
+            &user.pubkey(),
+            &recipient.pubkey(),
+            &lido_accounts.mint_program.pubkey(),
+            &lido_accounts.authority,
+            &lido_accounts.reserve.pubkey(),
+            TEST_DEPOSIT_AMOUNT,
+        )
+        .unwrap()],
+        Some(&payer.pubkey()),
+    );
+    transaction.sign(&[&payer, &user], recent_blockhash);
+    banks_client.process_transaction(transaction).await.unwrap();
+
+    let balance = get_token_balance(&mut banks_client, &recipient.pubkey()).await;
+    assert_eq!(balance, TEST_DEPOSIT_AMOUNT);
+}
