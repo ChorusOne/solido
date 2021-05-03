91ac8328802d589bee325d97ca2a23f4890e7743
E. Fynn <me@enriquefynn.com>
2021-05-03 21:24:13 +0200

    WIP: solido cmd client

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,3 +1,5 @@
+use solana_program::pubkey::Pubkey;
+
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
 pub mod error;
@@ -14,3 +16,13 @@ pub const DEPOSIT_AUTHORITY_ID: &[u8] = b"deposit_authority";
 pub const STAKE_POOL_TOKEN_RESERVE_AUTHORITY_ID: &[u8] = b"token_reserve_authority";
 
 solana_program::declare_id!("LidoB9L9nR3CrcaziKVYVpRX6A9Y1LAXYasjjfCbApj");
+
+pub fn find_deposit_authority_program_address(
+    program_id: &Pubkey,
+    lido_address: &Pubkey,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[&lido_address.to_bytes()[..32], DEPOSIT_AUTHORITY_ID],
+        program_id,
+    )
+}
