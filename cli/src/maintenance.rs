9e3dcc842fe98bdf5e3602e371fe47f1af19df2b
Ruud van Asseldonk <ruud@chorus.one>
2021-06-09 15:55:18 +0200

    Check for fully active stake in maintenance command
    This does not yet act on it, it only detects it.

--- cli/src/maintenance.rs
+++ cli/src/maintenance.rs
@@ -4,12 +4,13 @@ use crate::helpers::{get_solido, get_stake_pool};
 use crate::{Config, Error};
 use clap::Clap;
 use lido::{state::{Lido, Validator}, token::Lamports};
-use solana_program::{pubkey::Pubkey, rent::Rent, sysvar};
+use solana_program::{pubkey::Pubkey, rent::Rent, clock::Clock, stake_history::StakeHistory, sysvar};
 use solana_sdk::{account::Account, borsh::try_from_slice_unchecked, instruction::Instruction};
-use solana_stake_program::stake_state::StakeState;
 use solana_client::rpc_client::RpcClient;
 use spl_stake_pool::state::{StakePool, ValidatorList};
+use spl_stake_pool::stake_program::StakeState;
 use lido::state::PubkeyAndEntry;
+use borsh::BorshDeserialize;
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -49,24 +50,84 @@ struct SolidoState {
     validator_list: ValidatorList,
 
     /// For each validator, in the same order as in `solido.validators`, holds
-    /// the derived stake accounts from the begin seed until end seed.
-    validator_stake_accounts: Vec<Vec<(Pubkey, Account)>>,
+    /// the stake balance of the derived stake accounts from the begin seed until
+    /// end seed.
+    validator_stake_accounts: Vec<Vec<(Pubkey, StakeBalance)>>,
 
     reserve_account: Account,
     rent: Rent,
+}
+
+/// The balance of a stake account, split into the four states that stake can be in.
+///
+/// The sum of the four fields is equal to the SOL balance of the stake account.
+struct StakeBalance {
+    inactive: Lamports,
+    activating: Lamports,
+    active: Lamports,
+    deactivating: Lamports,
+}
+
+impl StakeBalance {
+    pub fn is_fully_active(&self) -> bool {
+        true
+        && self.inactive == Lamports(0)
+        && self.activating == Lamports(0)
+        && self.deactivating == Lamports(0)
+        // We define an empty stake account to not be fully active,
+        // because the SPL stake pool program considers such accounts
+        // to not be fully active. The case is not relevant anyway,
+        // because an empty stake account would not be rent-exempt,
+        // so it would disappear.
+        && self.active > Lamports(0)
+    }
 }
 
 fn get_validator_stake_accounts(
     rpc_client: &RpcClient,
     solido_program_id: &Pubkey,
     solido_address: &Pubkey,
+    clock: &Clock,
+    stake_history: &StakeHistory,
     validator: &PubkeyAndEntry<Validator>,
-) -> Result<Vec<(Pubkey, Account)>> {
+) -> Result<Vec<(Pubkey, StakeBalance)>> {
     let mut result = Vec::new();
     for seed in validator.entry.stake_accounts_seed_begin..validator.entry.stake_accounts_seed_end {
         let (addr, _bump_seed) = Validator::find_stake_account_address(solido_program_id, solido_address, &validator.pubkey, seed);
         let account = rpc_client.get_account(&addr)?;
-        result.push((addr, account));
+        let stake_state = StakeState::try_from_slice(&account.data)
+            .expect("Derived stake account contains invalid data.");
+        let delegation = stake_state.delegation()
+            .expect("Encountered undelegated stake account, this should not happen.");
+
+        // TODO: Confirm that `delegation.voter_pubkey == validator.vote_account`. Unfortunately we
+        // do not store the vote account in there at the moment. Once we switch to using the vote
+        // account as key for the dictionary, we could check that here.
+
+        let target_epoch = clock.epoch;
+        let history = Some(stake_history);
+        // TODO(ruuda): Confirm the meaning of this.
+        let fix_stake_deactivate = true;
+
+        let (active_lamports, activating_lamports, deactivating_lamports) = delegation
+            .stake_activating_and_deactivating(target_epoch, history, fix_stake_deactivate);
+
+        let inactive_lamports = account.lamports
+            .checked_sub(active_lamports)
+            .expect("Active stake cannot be larger than stake account balance.")
+            .checked_sub(activating_lamports)
+            .expect("Activating stake cannot be larger than stake account balance - active.")
+            .checked_sub(deactivating_lamports)
+            .expect("Deactivating stake cannot be larger than stake account balance - active - activating.");
+
+        let balance = StakeBalance {
+            inactive: Lamports(inactive_lamports),
+            activating: Lamports(activating_lamports),
+            active: Lamports(active_lamports),
+            deactivating: Lamports(deactivating_lamports),
+        };
+
+        result.push((addr, balance));
     }
     Ok(result)
 }
@@ -95,9 +156,15 @@ impl SolidoState {
         let rent_account = rpc.get_account(&sysvar::rent::ID)?;
         let rent: Rent = bincode::deserialize(&rent_account.data)?;
 
+        let clock_account = config.rpc().get_account(&sysvar::clock::ID)?;
+        let clock: Clock = bincode::deserialize(&clock_account.data)?;
+
+        let stake_history_account = config.rpc().get_account(&sysvar::stake_history::ID)?;
+        let stake_history: StakeHistory = bincode::deserialize(&stake_history_account.data)?;
+
         let mut validator_stake_accounts = Vec::new();
         for validator in solido.validators.entries.iter() {
-            validator_stake_accounts.push(get_validator_stake_accounts(rpc, solido_program_id, solido_address, validator)?);
+            validator_stake_accounts.push(get_validator_stake_accounts(rpc, solido_program_id, solido_address, &clock, &stake_history, validator)?);
         }
 
         Ok(SolidoState {
@@ -125,7 +192,9 @@ impl SolidoState {
     pub fn try_stake_deposit(&self) -> Result<Option<Instruction>> {
         let reserve_balance = self.get_effective_reserve();
         let minimum_stake_account_balance =
-            Lamports(StakeState::get_rent_exempt_reserve(&self.rent));
+            Lamports(
+                self.rent.minimum_balance(std::mem::size_of::<StakeState>())
+            );
 
         // If there is not enough reserve to create a new stake account, we
         // can't stake the deposit, even if there is some balance.
@@ -141,7 +210,14 @@ impl SolidoState {
     /// If there is active stake that can be deposited to the stake pool,
     /// return the instruction to do so.
     pub fn try_deposit_active_stake_to_pool(&self) -> Result<Option<Instruction>> {
-        // TODO: Check all stake accounts.
+        for (_validator, stake_accounts) in
+            self.solido.validators.entries.iter().zip(self.validator_stake_accounts.iter()) {
+            for (_stake_account_addr, stake_balance) in stake_accounts.iter() {
+                if stake_balance.is_fully_active() {
+                    // TODO: Generate DepositActiveStake instruction.
+                }
+            }
+        }
         Ok(None)
     }
 }
