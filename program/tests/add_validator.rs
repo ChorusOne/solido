6ea5460930a428f00cc0958afd0e3d1f94d4a2ca
Ruud van Asseldonk <ruud@chorus.one>
2021-06-17 16:02:31 +0200

    Make tests compile after changing validator key
    I changed the key for the validators map from thes take pool stake
    account to the validator vote account. This has far-fetching
    consequences, so it required invasive changes in the tests too.
    
    A few tests are no longer relevant. The update balance test was a subset
    of the fee distribution test, so that one was also redundant. But the
    actual fee distribution test, I had partially disable for now, because
    we don't yet have fee distribution through our stake accounts.

--- program/tests/add_validator.rs
+++ program/tests/add_validator.rs
@@ -4,10 +4,9 @@ mod helpers;
 
 use helpers::{get_account, program_test, simple_add_validator_to_pool, LidoAccounts};
 use lido::state::Lido;
-use solana_program::{borsh::try_from_slice_unchecked, hash::Hash, native_token::sol_to_lamports};
+use solana_program::{borsh::try_from_slice_unchecked, hash::Hash};
 use solana_program_test::{tokio, BanksClient};
 use solana_sdk::signature::{Keypair, Signer};
-use spl_stake_pool::stake_program;
 
 async fn setup() -> (BanksClient, Keypair, Hash, LidoAccounts) {
     let (mut banks_client, payer, last_blockhash) = program_test().start().await;
@@ -23,17 +22,15 @@ async fn setup() -> (BanksClient, Keypair, Hash, LidoAccounts) {
 async fn test_successful_add_validator() {
     let (mut banks_client, payer, last_blockhash, lido_accounts) = setup().await;
 
-    let validator_stake =
+    let accounts =
         simple_add_validator_to_pool(&mut banks_client, &payer, &last_blockhash, &lido_accounts)
             .await;
 
     let lido_account = get_account(&mut banks_client, &lido_accounts.lido.pubkey()).await;
     let lido = try_from_slice_unchecked::<Lido>(lido_account.data.as_slice()).unwrap();
 
-    let has_stake_account = lido
-        .validators
-        .get(&validator_stake.stake_pool_stake_account)
-        .is_ok();
+    let has_stake_account = lido.validators.get(&accounts.vote_account.pubkey()).is_ok();
+
     // Validator is inside the credit structure
     assert!(has_stake_account);
 
@@ -41,29 +38,10 @@ async fn test_successful_add_validator() {
         .validators
         .entries
         .iter()
-        .any(|pe| pe.entry.fee_address == validator_stake.validator_token_account.pubkey());
+        .any(|pe| pe.entry.fee_address == accounts.fee_account.pubkey());
+
     // Validator token account is the same one as provided
     assert!(has_token_account);
 
-    assert_eq!(lido.validators.len(), 1,);
-
-    let stake_account =
-        get_account(&mut banks_client, &validator_stake.stake_pool_stake_account).await;
-    let stake_account =
-        try_from_slice_unchecked::<stake_program::StakeState>(&stake_account.data).unwrap();
-    let (meta, _) = match stake_account {
-        stake_program::StakeState::Stake(meta, stake) => (meta, stake),
-        _ => panic!(),
-    };
-    let balance = banks_client
-        .get_balance(validator_stake.stake_pool_stake_account)
-        .await
-        .unwrap();
-    let rent = banks_client.get_rent().await.unwrap();
-    let rent_exempt_validator_stake =
-        rent.minimum_balance(std::mem::size_of::<stake_program::StakeState>());
-    // Sanity check
-    assert_eq!(rent_exempt_validator_stake, meta.rent_exempt_reserve);
-    // Stake account balance should have 1 Sol + rent exempt
-    assert_eq!(balance, sol_to_lamports(1.) + rent_exempt_validator_stake);
+    assert_eq!(lido.validators.len(), 1);
 }
--- program/tests/stake_deposit.rs
+++ program/tests/stake_deposit.rs
@@ -2,28 +2,17 @@
 
 mod helpers;
 
-use borsh::BorshDeserialize;
 use helpers::{
-    get_account, program_test, simple_add_validator_to_pool,
-    stakepool_account::{get_token_balance, ValidatorStakeAccount},
+    get_account, program_test, simple_add_validator_to_pool, stakepool_account::ValidatorAccounts,
     LidoAccounts,
 };
-use lido::token::{Lamports, StakePoolTokenLamports};
+use lido::state::Lido;
+use lido::token::Lamports;
 use solana_program::{borsh::try_from_slice_unchecked, hash::Hash};
 use solana_program_test::{tokio, BanksClient};
 use solana_sdk::signature::{Keypair, Signer};
-use spl_stake_pool::{
-    minimum_stake_lamports, stake_program,
-    state::{StakePool, ValidatorList},
-};
 
-async fn setup() -> (
-    BanksClient,
-    Keypair,
-    Hash,
-    LidoAccounts,
-    Vec<ValidatorStakeAccount>,
-) {
+async fn setup() -> (BanksClient, Keypair, Hash, LidoAccounts, ValidatorAccounts) {
     let (mut banks_client, payer, recent_blockhash) = program_test().start().await;
     let mut lido_accounts = LidoAccounts::new();
     lido_accounts
@@ -40,15 +29,26 @@ async fn setup() -> (
         payer,
         recent_blockhash,
         lido_accounts,
-        vec![validator],
+        validator,
     )
 }
 pub const TEST_DEPOSIT_AMOUNT: Lamports = Lamports(100_000_000_000);
 pub const TEST_STAKE_DEPOSIT_AMOUNT: Lamports = Lamports(10_000_000_000);
 
 #[tokio::test]
-async fn test_successful_stake_deposit_stake_pool_deposit() {
-    let (mut banks_client, payer, recent_blockhash, lido_accounts, stake_accounts) = setup().await;
+async fn test_successful_stake_deposit() {
+    let (mut banks_client, payer, recent_blockhash, lido_accounts, validator_accounts) =
+        setup().await;
+
+    // Sanity check before we start: the validator should have zero balance in zero stake accounts.
+    let solido_account = get_account(&mut banks_client, &lido_accounts.lido.pubkey()).await;
+    let solido_before = try_from_slice_unchecked::<Lido>(solido_account.data.as_slice()).unwrap();
+    let validator_before = &solido_before.validators.entries[0].entry;
+    assert_eq!(validator_before.stake_accounts_balance, Lamports(0));
+    assert_eq!(validator_before.stake_accounts_seed_begin, 0);
+    assert_eq!(validator_before.stake_accounts_seed_end, 0);
+
+    // Now we make a deposit, and then delegate part of it.
     lido_accounts
         .deposit(
             &mut banks_client,
@@ -58,107 +58,42 @@ async fn test_successful_stake_deposit_stake_pool_deposit() {
         )
         .await;
 
-    // Delegate the deposit
-    let validator_account = stake_accounts.get(0).unwrap();
     let stake_account = lido_accounts
         .stake_deposit(
             &mut banks_client,
             &payer,
             &recent_blockhash,
-            validator_account,
+            &validator_accounts.vote_account.pubkey(),
             TEST_STAKE_DEPOSIT_AMOUNT,
         )
         .await;
 
-    let stake_pool_before = get_account(
-        &mut banks_client,
-        &lido_accounts.stake_pool_accounts.stake_pool.pubkey(),
-    )
-    .await;
-    let stake_pool_before = StakePool::try_from_slice(&stake_pool_before.data.as_slice()).unwrap();
-
-    let validator_list = get_account(
-        &mut banks_client,
-        &lido_accounts.stake_pool_accounts.validator_list.pubkey(),
-    )
-    .await;
-
-    let validator_list =
-        try_from_slice_unchecked::<ValidatorList>(validator_list.data.as_slice()).unwrap();
-    let validator_stake_item_before = validator_list
-        .find(&validator_account.vote.pubkey())
-        .unwrap();
-
-    lido_accounts
-        .deposit_active_stake_to_pool(
-            &mut banks_client,
-            &payer,
-            &recent_blockhash,
-            validator_account,
-            &stake_account,
-        )
-        .await;
-
-    // Stake pool should add its balance to the pool balance
-    let stake_pool = get_account(
-        &mut banks_client,
-        &lido_accounts.stake_pool_accounts.stake_pool.pubkey(),
-    )
-    .await;
-    let stake_pool = StakePool::try_from_slice(&stake_pool.data.as_slice()).unwrap();
+    // The amount that we staked, should now be in the stake account.
     assert_eq!(
-        Some(Lamports(stake_pool.total_stake_lamports)),
-        Lamports(stake_pool_before.total_stake_lamports) + TEST_STAKE_DEPOSIT_AMOUNT
-    );
-    // In general we can't add the deposit in SOL and the token supply in stake
-    // pool tokens, but in this case, the exchange rate is 1.
-    assert_eq!(
-        StakePoolTokenLamports(stake_pool.pool_token_supply),
-        StakePoolTokenLamports(stake_pool_before.pool_token_supply + TEST_STAKE_DEPOSIT_AMOUNT.0)
+        Lamports(
+            get_account(&mut banks_client, &stake_account)
+                .await
+                .lamports
+        ),
+        TEST_STAKE_DEPOSIT_AMOUNT,
     );
 
-    // Check minted tokens
-    let lido_token_balance = StakePoolTokenLamports(
-        get_token_balance(
-            &mut banks_client,
-            &lido_accounts.stake_pool_token_holder.pubkey(),
-        )
-        .await,
-    );
-    // In general we can't compare stake pool tokens to SOL, but in this case,
-    // the exchange rate is 1.
-    assert_eq!(lido_token_balance.0, TEST_STAKE_DEPOSIT_AMOUNT.0);
+    // We should also have recorded in the Solido state that this validator now
+    // has balance in a stake account.
+    let solido_account = get_account(&mut banks_client, &lido_accounts.lido.pubkey()).await;
+    let solido_after = try_from_slice_unchecked::<Lido>(solido_account.data.as_slice()).unwrap();
 
-    // Check balances in validator stake account list storage
-    let validator_list = get_account(
-        &mut banks_client,
-        &lido_accounts.stake_pool_accounts.validator_list.pubkey(),
-    )
-    .await;
-    let validator_list =
-        try_from_slice_unchecked::<ValidatorList>(validator_list.data.as_slice()).unwrap();
-    let validator_stake_item = validator_list
-        .find(&validator_account.vote.pubkey())
-        .unwrap();
+    let validator_after = &solido_after.validators.entries[0].entry;
     assert_eq!(
-        Some(Lamports(validator_stake_item.stake_lamports)),
-        Lamports(validator_stake_item_before.stake_lamports) + TEST_STAKE_DEPOSIT_AMOUNT
+        validator_after.stake_accounts_balance,
+        TEST_STAKE_DEPOSIT_AMOUNT
     );
 
-    // Check validator stake account actual SOL balance
-    let validator_stake_account = get_account(
-        &mut banks_client,
-        &validator_account.stake_pool_stake_account,
-    )
-    .await;
-    let stake_state =
-        bincode::deserialize::<stake_program::StakeState>(&validator_stake_account.data).unwrap();
-    let meta = stake_state.meta().unwrap();
-    assert_eq!(
-        Lamports(validator_stake_account.lamports) - Lamports(minimum_stake_lamports(&meta)),
-        Some(Lamports(validator_stake_item.stake_lamports))
-    );
+    // This was also the first deposit, so that should have created one stake account.
+    assert_eq!(validator_after.stake_accounts_seed_begin, 0);
+    assert_eq!(validator_after.stake_accounts_seed_end, 1);
 }
+
 #[tokio::test]
 // TODO(#187) Implement test for stake_exists_stake_deposit
 async fn test_stake_exists_stake_deposit() {}
