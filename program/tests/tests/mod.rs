8e5a95d0c5cf28b6dba14da4a00d2dd79a269af2
Ruud van Asseldonk <ruud@chorus.one>
2021-06-30 13:32:26 +0200

    Add UpdateValidatorBalance to test context

--- program/tests/tests/mod.rs
+++ program/tests/tests/mod.rs
@@ -1,7 +1,7 @@
 pub mod add_remove_validator;
 pub mod change_reward_distribution;
 pub mod deposit;
-pub mod fee_distribution;
+pub mod update_validator_balance;
 pub mod maintainers;
 pub mod merge_stake;
 pub mod stake_deposit;
