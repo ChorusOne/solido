67d46ab7f2e7e04e28b342391bddaf25d79248f3
E. Fynn <fynn@chorus.one>
2021-07-25 10:11:32 +0200

    Split commits to another PR.

--- program/src/error.rs
+++ program/src/error.rs
@@ -14,142 +14,142 @@ use crate::token::ArithmeticError;
 pub enum LidoError {
     /// Address is already initialized
     #[error("AlreadyInUse")]
-    AlreadyInUse = 0,
+    AlreadyInUse,
     /// Lido account mismatch the one stored in the Lido program
     #[error("InvalidOwner")]
-    InvalidOwner = 1,
+    InvalidOwner,
     /// Invalid allocated amount
     #[error("InvalidAmount")]
-    InvalidAmount = 2,
+    InvalidAmount,
     /// A required signature is missing
     #[error("SignatureMissing")]
-    SignatureMissing = 3,
+    SignatureMissing,
     /// The reserve account is invalid
     #[error("InvalidReserveAccount")]
-    InvalidReserveAccount = 4,
+    InvalidReserveAccount,
     /// Calculation failed due to division by zero or overflow
     #[error("CalculationFailure")]
-    CalculationFailure = 5,
+    CalculationFailure,
     #[error("WrongStakeState")]
     /// Stake account does not exist or is in an invalid state
-    WrongStakeState = 6,
+    WrongStakeState,
     /// The sum of numerators should be equal to the denominators
     #[error("InvalidFeeAmount")]
-    InvalidFeeAmount = 7,
+    InvalidFeeAmount,
     /// Number of maximum validators reached
     #[error("InvalidFeeAmount")]
-    MaximumNumberOfAccountsExceeded = 8,
+    MaximumNumberOfAccountsExceeded,
     /// The size of the account for the Solido state does not match `max_validators`.
     #[error("UnexpectedMaxValidators")]
-    UnexpectedMaxValidators = 9,
+    UnexpectedMaxValidators,
     /// Wrong manager trying  to alter the state
     #[error("InvalidManager")]
-    InvalidManager = 10,
+    InvalidManager,
     /// Wrong maintainer trying  to alter the state
     #[error("InvalidMaintainer")]
-    InvalidMaintainer = 11,
+    InvalidMaintainer,
     /// One of the provided accounts had a mismatch in is_writable or is_signer,
     /// or for a const account, the address does not match the expected address.
     #[error("InvalidAccountInfo")]
-    InvalidAccountInfo = 12,
+    InvalidAccountInfo,
     /// More accounts were provided than the program expects.
     #[error("TooManyAccountKeys")]
-    TooManyAccountKeys = 13,
+    TooManyAccountKeys,
     /// Wrong fee distribution account
     #[error("InvalidFeeDistributionAccount")]
-    InvalidFeeDistributionAccount = 14,
+    InvalidFeeDistributionAccount,
     /// Wrong validator credits account
     #[error("InvalidValidatorCreditAccount")]
-    InvalidValidatorCreditAccount = 15,
+    InvalidValidatorCreditAccount,
     /// Validator credit account was changed
     #[error("ValidatorCreditChanged")]
-    ValidatorCreditChanged = 16,
+    ValidatorCreditChanged,
     /// Fee account should be the same as the Stake pool fee'
     #[error("ValidatorCreditChanged")]
-    InvalidFeeAccount = 17,
+    InvalidFeeAccount,
     /// One of the fee recipients is invalid
     #[error("InvalidFeeRecipient")]
-    InvalidFeeRecipient = 18,
+    InvalidFeeRecipient,
     /// There is a stake account with the same key present in the validator
     /// credit list.
     #[error("DuplicatedEntry")]
-    DuplicatedEntry = 19,
+    DuplicatedEntry,
     /// Validator credit account was not found
     #[error("ValidatorCreditNotFound")]
-    ValidatorCreditNotFound = 20,
+    ValidatorCreditNotFound,
     /// Validator has unclaimed credit, should mint the tokens before the validator removal
     #[error("ValidatorHasUnclaimedCredit")]
-    ValidatorHasUnclaimedCredit = 21,
+    ValidatorHasUnclaimedCredit,
     /// The reserve account is not rent exempt
     #[error("ReserveIsNotRentExempt")]
-    ReserveIsNotRentExempt = 22,
+    ReserveIsNotRentExempt,
     /// The requested amount for reserve withdrawal exceeds the maximum held in
     /// the reserve account considering rent exemption
     #[error("AmountExceedsReserve")]
-    AmountExceedsReserve = 23,
+    AmountExceedsReserve,
     /// The same maintainer's public key already exists in the structure
     #[error("DuplicatedMaintainer")]
-    DuplicatedMaintainer = 24,
+    DuplicatedMaintainer,
     /// A member of the accounts list (maintainers or validators) is not present
     /// in the structure
     #[error("InvalidAccountMember")]
-    InvalidAccountMember = 25,
+    InvalidAccountMember,
     /// Lido has an invalid size, calculated with the Lido's constant size plus
     /// required to hold variable structures
     #[error("InvalidAccountMember")]
-    InvalidLidoSize = 26,
+    InvalidLidoSize,
     /// There are no validators with an active stake account to delegate to.
     #[error("NoActiveValidators")]
-    NoActiveValidators = 27,
+    NoActiveValidators,
 
     /// When staking part of the reserve to a new stake account, the next
     /// program-derived address for the stake account associated with the given
     /// validator, does not match the provided stake account, or the stake account
     /// is not the right account to stake with at this time.
     #[error("InvalidStakeAccount")]
-    InvalidStakeAccount = 28,
+    InvalidStakeAccount,
 
     /// We expected an SPL token account that holds stSOL,
     /// but this was not an SPL token account,
     /// or its mint did not match.
     #[error("InvalidStSolAccount")]
-    InvalidStSolAccount = 29,
+    InvalidStSolAccount,
 
     /// The exchange rate has already been updated this epoch.
     #[error("ExchangeRateAlreadyUpToDate")]
-    ExchangeRateAlreadyUpToDate = 30,
+    ExchangeRateAlreadyUpToDate,
 
     /// The exchange rate has not yet been updated this epoch.
     #[error("ExchangeRateNotUpdatedInThisEpoch")]
-    ExchangeRateNotUpdatedInThisEpoch = 31,
+    ExchangeRateNotUpdatedInThisEpoch,
 
     /// We observed a decrease in the balance of the validator's stake accounts.
     #[error("ValidatorBalanceDecreased")]
-    ValidatorBalanceDecreased = 32,
+    ValidatorBalanceDecreased,
 
     /// The provided stake authority does not match the one derived from Lido's state.
     #[error("InvalidStakeAuthority")]
-    InvalidStakeAuthority = 33,
+    InvalidStakeAuthority,
 
     /// The provided rewards withdraw authority does not match the one derived from Lido's state.
     #[error("InvalidRewardsWithdrawAuthority")]
-    InvalidRewardsWithdrawAuthority = 34,
+    InvalidRewardsWithdrawAuthority,
 
     /// The provided Vote Account is invalid or corrupted.
     #[error("InvalidVoteAccount")]
-    InvalidVoteAccount = 35,
+    InvalidVoteAccount,
 
     /// The provided token owner is different from the given one.
     #[error("InvalidTokenOwner")]
-    InvalidTokenOwner = 36,
+    InvalidTokenOwner,
 
     /// Token was delegated to an unexpected account.
     #[error("InvalidTokenDelegation")]
-    InvalidTokenDelegation = 37,
+    InvalidTokenDelegation,
 
     /// There is a validator that has more stake than the selected one.
     #[error("ValidatorWithMoreStakeExists")]
-    ValidatorWithMoreStakeExists = 38,
+    ValidatorWithMoreStakeExists,
 }
 
 impl From<ArithmeticError> for LidoError {
