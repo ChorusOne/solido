e4ac606f1af16dd572e59148cb8223f3d98b1f73
E. Fynn <fynn@chorus.one>
2021-08-04 09:22:00 -0300

    Nicer msg, exit on total fail.

--- tests/airdrop_lamports.sh
+++ tests/airdrop_lamports.sh
@@ -6,11 +6,13 @@ do
     then
         break
     else
-        echo "Airdrop failed, awaiting 2s before asking again..."
-        sleep 2
         if [ $i -eq 5 ]
         then
             echo "Airdrop failed after trying 5 times, giving up."
+            exit 1
+        else
+            echo "Airdrop failed, awaiting 2s before asking again..."
+            sleep 2
         fi
     fi
 done
