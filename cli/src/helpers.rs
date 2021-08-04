f6f03b88e69c949b5910e256966446af57969854
Ruud van Asseldonk <ruud@chorus.one>
2021-08-04 16:44:55 +0200

    Fix type error

--- cli/src/helpers.rs
+++ cli/src/helpers.rs
@@ -106,7 +106,7 @@ pub fn command_create_solido(
     let lido_signer = {
         if opts.solido_key_path() != &PathBuf::default() {
             // If we've been given a solido private key, use it to create the solido instance.
-            get_signer(opts.solido_key_path().clone())
+            get_signer(opts.solido_key_path().clone())?
         } else {
             // If not, use a random key
             Box::new(Keypair::new())
