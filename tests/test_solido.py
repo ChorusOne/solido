8ec3a09352018e7a8d9394f954ffdde35a8ca7cd
E. Fynn <fynn@chorus.one>
2021-08-20 11:26:45 -0300

    Fix tests.

--- tests/test_solido.py
+++ tests/test_solido.py
@@ -306,6 +306,10 @@ assert solido_instance['solido']['validators']['entries'][0] == {
             'stake_accounts_seed_begin': 0,
             'stake_accounts_seed_end': 0,
         },
+        'stake_seeds': {
+            'stake_accounts_seed_begin': 0,
+            'stake_accounts_seed_end': 0,
+        },
         'stake_accounts_balance': 0,
         'active': True,
     },
