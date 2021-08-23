020d165c38cfb4da5946c6ee0f868668b9643671
Ruud van Asseldonk <ruud@chorus.one>
2021-08-23 17:24:53 +0200

    Deploy website - based on 9c093eae11857a10a872717306aa71faaac8a79e

---
+++ assets/js/4956000d.1e1a1db2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[7605],{6353:function(s){s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"test blog post","permalink":"/solido/blog/test"}]}')}}]);
---
+++ assets/js/869851b0.30f28d1b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1234],{7659:function(e){e.exports=JSON.parse('{"permalink":"/solido/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
