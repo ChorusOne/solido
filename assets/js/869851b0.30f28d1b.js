c62651a05acacf1adf8ae65af5db7b6548ceac9e
Rishi Sidhu <rishi@chorus.one>
2021-09-07 22:05:13 +0530

    Deploy website - based on 156aafb4a88982734150b2f60156609636f813d8

--- assets/js/869851b0.30f28d1b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1234],{7659:function(e){e.exports=JSON.parse('{"permalink":"/solido/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/cc773e5e.b4e92141.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[5682],{6353:function(s){"use strict";s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"test blog post","permalink":"/solido/blog/test"}]}')}}]);
