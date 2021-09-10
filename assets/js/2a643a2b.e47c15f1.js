c616fe9c670fbdb08217476b6ca2460be4c58964
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2021-09-10 10:11:46 +0100

    Deploy website - based on 8611c0bb58af6a688c315aa24d996e7d67d2e36f

---
+++ assets/js/2a643a2b.e47c15f1.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[6441],{6353:function(t){"use strict";t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"test blog post","permalink":"/blog/test"}]}')}}]);
---
+++ assets/js/5d7eaf65.03af1a37.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[3082],{7725:function(s){"use strict";s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"chorusone","name":"chorusone","count":1,"permalink":"/blog/tags/chorusone"}')}}]);
