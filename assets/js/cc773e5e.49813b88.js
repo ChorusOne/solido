32437174cbb6af45137950f8f9209484ad47c1a5
Rishi Sidhu <rishi@chorus.one>
2021-09-21 18:08:55 +0530

    Deploy website - based on 73d6759251137dec8b2843faa4432face539e1ae

---
+++ assets/js/cc773e5e.49813b88.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[5682],{6353:function(t){"use strict";t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"test blog post","permalink":"/blog/test"}]}')}}]);
