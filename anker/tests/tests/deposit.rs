fef43c505fd4484ec241460ccca320c12bc1ffbf
Ruud van Asseldonk <ruud@chorus.one>
2021-11-24 13:39:58 +0100

    Test that Anker::Deposit fails with wrong reserve
    This is a bug that we had in the original Solido program. We added a
    regression test for it there. We should add the same test here. Don't
    make the same mistake twice. (The test fails, so we did not make the
    mistake, but it is still good to confirm.)

--- anker/tests/tests/deposit.rs
+++ anker/tests/tests/deposit.rs
@@ -2,9 +2,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use anker::token::BLamports;
+use anker::error::AnkerError;
 use lido::token::{Lamports, StLamports};
 use solana_program_test::tokio;
+use solana_sdk::signer::Signer;
 use testlib::anker_context::Context;
+use testlib::assert_solido_error;
 
 const TEST_DEPOSIT_AMOUNT: StLamports = StLamports(1_000_000_000);
 
@@ -37,4 +40,19 @@ async fn test_successful_deposit_different_exchange_rate() {
     // The exchange rate is now 1:2.
     assert_eq!(reserve_balance, StLamports(500_000_000));
     assert_eq!(recipient_balance, BLamports(TEST_DEPOSIT_AMOUNT.0));
+}
+
+#[tokio::test]
+async fn test_deposit_fails_with_wrong_reserve() {
+    let mut context = Context::new().await;
+
+    let fake_reserve = context.solido_context.deterministic_keypair.new_keypair();
+    context.reserve = fake_reserve.pubkey();
+
+    // The program should confirm that the reserve we use is the reserve of the
+    // instance, and fail the transaction if it's a different account. Otherwise
+    // we could pass in a reserve controlled by us (where we are an attacker), and
+    // get bSOL while also retaining the stSOL.
+    let result = context.try_deposit(Lamports(TEST_DEPOSIT_AMOUNT.0)).await;
+    assert_solido_error!(result, AnkerError::InvalidDerivedAccount);
 }
