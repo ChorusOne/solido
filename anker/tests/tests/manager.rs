ea71b93fc6f7353f8e83acc21c79ccad7ff22529
E. Fynn <fynn@chorus.one>
2021-12-13 09:00:02 -0300

    Update tests

--- anker/tests/tests/manager.rs
+++ anker/tests/tests/manager.rs
@@ -91,7 +91,8 @@ async fn test_change_token_swap_pool_different_manager() {
 #[tokio::test]
 async fn test_successful_change_terra_rewards_destination() {
     let mut context = Context::new().await;
-    let new_terra_rewards_address = TerraAddress::from_str("0xdeadbeef").unwrap();
+    let new_terra_rewards_address =
+        TerraAddress::from_str("terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3").unwrap();
     let manager = Keypair::from_bytes(&context.solido_context.manager.to_bytes()).unwrap();
     let result = context
         .try_change_terra_rewards_destination(&manager, new_terra_rewards_address.clone())
@@ -104,7 +105,8 @@ async fn test_successful_change_terra_rewards_destination() {
 #[tokio::test]
 async fn test_change_terra_rewards_destination_different_manager() {
     let mut context = Context::new().await;
-    let new_terra_rewards_address = TerraAddress::from_str("0xdeadbeef").unwrap();
+    let new_terra_rewards_address =
+        TerraAddress::from_str("terra1fex9f78reuwhfsnc8sun6mz8rl9zwqh03fhwf3").unwrap();
     let wrong_manager = context.solido_context.deterministic_keypair.new_keypair();
     let anker = context.get_anker().await;
     let result = context
