6bb8d1a45cdd5c4f3ab653d51c93fa68865b54a9
Hritique Rungta <hritiquerungta@gmail.com>
2022-02-08 13:32:57 +0530

    Added anker deposit and withdraw instructions

---
+++ js/src/instructions/anker/deposit.ts
@@ -0,0 +1,90 @@
+import * as BufferLayout from '@solana/buffer-layout';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { PublicKey, TransactionInstruction } from '@solana/web3.js';
+import { ProgramAddresses, StLamports } from '../../types';
+
+/**
+ * Returns the instruction to deposit stSOL into the Anker program and mint bSOL tokens
+ * @param senderStSolAccountOwnerAddress Address of the owner of the sender's stSOL SPL token account
+ * @param senderStSolAccountAddress Address of the stSOL SPL token account, whose stSOL balance will be decreased
+ * @param recipientBSolAccountAddress Address of the recipient bSOL SPL token account
+ * @param programAddresses Solido and Anker program addresses
+ * @param amount Amount of stSOL to be deposited
+ */
+export const getDepositInstruction = async (
+  senderStSolAccountOwnerAddress: PublicKey,
+  senderStSolAccountAddress: PublicKey,
+  recipientBSolAccountAddress: PublicKey,
+  programAddresses: ProgramAddresses,
+  amount: StLamports
+) => {
+  const [stSolReserveAccountAddress] = await PublicKey.findProgramAddress(
+    [
+      programAddresses.ankerInstanceId.toBuffer(),
+      Buffer.from('st_sol_reserve_account'),
+    ],
+    programAddresses.ankerProgramId
+  );
+
+  const [bSolMintAuthorityAddress] = await PublicKey.findProgramAddress(
+    [
+      programAddresses.ankerInstanceId.toBuffer(),
+      Buffer.from('mint_authority'),
+    ],
+    programAddresses.ankerProgramId
+  );
+
+  const keys = [
+    {
+      pubkey: programAddresses.ankerInstanceId,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: programAddresses.solidoInstanceId,
+      isSigner: false,
+      isWritable: false,
+    },
+    { pubkey: senderStSolAccountAddress, isSigner: false, isWritable: true },
+    {
+      pubkey: senderStSolAccountOwnerAddress,
+      isSigner: true,
+      isWritable: false,
+    },
+    {
+      pubkey: stSolReserveAccountAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: recipientBSolAccountAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: programAddresses.bSolMintAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: bSolMintAuthorityAddress,
+      isSigner: false,
+      isWritable: false,
+    },
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+  ];
+
+  const dataLayout = BufferLayout.struct([
+    BufferLayout.u8('instruction'),
+    BufferLayout.nu64('amount'),
+  ]);
+
+  const data = Buffer.alloc(dataLayout.span);
+  dataLayout.encode({ instruction: 1, amount: amount.stLamports }, data);
+
+  return new TransactionInstruction({
+    keys,
+    programId: programAddresses.ankerProgramId,
+    data,
+  });
+};
---
+++ js/src/instructions/anker/withdraw.ts
@@ -0,0 +1,90 @@
+import * as BufferLayout from '@solana/buffer-layout';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { PublicKey, TransactionInstruction } from '@solana/web3.js';
+import { BLamports, ProgramAddresses } from '../../types';
+
+/**
+ * Returns the instruction to withdraw stSOL from the Anker program
+ * @param senderBSolAccountOwnerAddress Address of the owner of the sender's bSOL SPL token account
+ * @param senderBSolAccountAddress Address of the bSOL SPL token account, whose bSOL balance will be decreased
+ * @param recipientStSolAccountAddress Address of the recipient bSOL SPL token account
+ * @param programAddresses Solido and Anker program addresses
+ * @param amount Amount of bSOL to be withdrawn
+ */
+export const getWithdrawInstruction = async (
+  senderBSolAccountOwnerAddress: PublicKey,
+  senderBSolAccountAddress: PublicKey,
+  recipientStSolAccountAddress: PublicKey,
+  programAddresses: ProgramAddresses,
+  amount: BLamports
+) => {
+  const [stSolReserveAccountAddress] = await PublicKey.findProgramAddress(
+    [
+      programAddresses.ankerInstanceId.toBuffer(),
+      Buffer.from('st_sol_reserve_account'),
+    ],
+    programAddresses.ankerProgramId
+  );
+
+  const [reserveAuthorityAddress] = await PublicKey.findProgramAddress(
+    [
+      programAddresses.ankerInstanceId.toBuffer(),
+      Buffer.from('reserve_authority'),
+    ],
+    programAddresses.ankerProgramId
+  );
+
+  const keys = [
+    {
+      pubkey: programAddresses.ankerInstanceId,
+      isSigner: false,
+      isWritable: false,
+    },
+    {
+      pubkey: programAddresses.solidoInstanceId,
+      isSigner: false,
+      isWritable: false,
+    },
+    { pubkey: senderBSolAccountAddress, isSigner: false, isWritable: true },
+    {
+      pubkey: senderBSolAccountOwnerAddress,
+      isSigner: true,
+      isWritable: false,
+    },
+    {
+      pubkey: recipientStSolAccountAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: stSolReserveAccountAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: reserveAuthorityAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    {
+      pubkey: programAddresses.bSolMintAddress,
+      isSigner: false,
+      isWritable: true,
+    },
+    { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+  ];
+
+  const dataLayout = BufferLayout.struct([
+    BufferLayout.u8('instruction'),
+    BufferLayout.nu64('amount'),
+  ]);
+
+  const data = Buffer.alloc(dataLayout.span);
+  dataLayout.encode({ instruction: 2, amount: amount.bLamports }, data);
+
+  return new TransactionInstruction({
+    keys,
+    programId: programAddresses.ankerProgramId,
+    data,
+  });
+};
