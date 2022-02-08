c57da3cbfd3f2cdd78dbdc57937593d65151f6cb
Hritique Rungta <hritiquerungta@gmail.com>
2022-02-08 13:52:00 +0530

    Exported anker functions from index.ts

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,4 +1,16 @@
-export { Solido, getSnapshot, getSolido } from './snapshot';
+export {
+  Solido,
+  getSnapshot as getSolidoSnapshot,
+  getSolido,
+} from './solidoSnapshot';
+
+// Anker
+export {
+  Anker,
+  getAnker,
+  getSnapshot as getAnkerSnapshot,
+} from './ankerSnapshot';
+
 export {
   getTotalValueLocked,
   getStSolSupply,
@@ -6,15 +18,28 @@ export {
   getTokenAccountsByOwner,
   getTotalNumberOfTokenAccounts,
 } from './stats';
+
+export { getTotalValueLocked as getTotalValueLockedInAnker } from './ankerStats';
+
 export {
   findAuthorityProgramAddress,
   exchangeSol,
   exchangeStSol,
 } from './utils';
-export { MAINNET_PROGRAM_ADDRESSES } from './constants';
 
-export { getDepositInstruction } from './instructions/deposit';
-export { getWithdrawInstruction } from './instructions/withdraw';
+export { convertBSolToStSol, convertStSolToBSol } from './ankerUtils';
+
+export {
+  MAINNET_PROGRAM_ADDRESSES,
+  DEVNET_PROGRAM_ADDRESSES,
+} from './constants';
+
+export { getDepositInstruction as getSolidoDepositInstruction } from './instructions/deposit';
+export { getWithdrawInstruction as getSolidoWithdrawInstruction } from './instructions/withdraw';
 export { getATAInitializeInstruction } from './instructions/utils';
+
+// Anker
+export { getDepositInstruction as getAnkerDepositInstruction } from './instructions/anker/deposit';
+export { getWithdrawInstruction as getAnkerWithdrawInstruction } from './instructions/anker/withdraw';
 
 export * from './types';
