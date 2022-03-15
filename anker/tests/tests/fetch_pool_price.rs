93580a464d052006b2d9a55e50602f6a2aa682b8
E. Fynn <fynn@chorus.one>
2022-03-15 15:22:47 -0300

    Add test when price change
    Add another call to `fetch_pool_price` that does not fail in
    `test_fail_fetch_pool_price_too_early` test.

--- anker/tests/tests/fetch_pool_price.rs
+++ anker/tests/tests/fetch_pool_price.rs
@@ -6,9 +6,10 @@ use anker::{
     state::{HistoricalStSolPrice, POOL_PRICE_MIN_SAMPLE_DISTANCE, POOL_PRICE_NUM_SAMPLES},
     token::MicroUst,
 };
-use lido::token::Lamports;
+use lido::token::{Lamports, StLamports};
 use solana_program::clock::DEFAULT_SLOTS_PER_EPOCH;
 use solana_program_test::tokio;
+use solana_sdk::signer::Signer;
 use testlib::{anker_context::Context, assert_solido_error};
 
 const DEPOSIT_AMOUNT: u64 = 1_000_000_000; // 1e9 units
@@ -54,6 +55,61 @@ async fn test_successful_fetch_pool_price() {
 }
 
 #[tokio::test]
+async fn test_fetch_pool_price_when_price_changed() {
+    let mut context = Context::new().await;
+    context
+        .initialize_token_pool_and_deposit(Lamports(DEPOSIT_AMOUNT))
+        .await;
+
+    // Deposit some tokens so we have StSol
+    let (st_sol_keypair, st_sol_token) = context
+        .solido_context
+        .deposit(Lamports(10_000_000_000))
+        .await;
+    let ust_address = context
+        .create_ust_token_account(st_sol_keypair.pubkey())
+        .await;
+
+    context.solido_context.advance_to_normal_epoch(2);
+    context.fetch_pool_price().await;
+
+    let amount_in = StLamports(1_000_000_000);
+    let min_amount_out = MicroUst(0);
+    context
+        .swap_st_sol_for_ust(
+            &st_sol_token,
+            &ust_address,
+            &st_sol_keypair,
+            amount_in,
+            min_amount_out,
+        )
+        .await;
+
+    context.solido_context.advance_to_normal_epoch(3);
+    context.fetch_pool_price().await;
+
+    let anker = context.get_anker().await;
+    assert_eq!(
+        anker.historical_st_sol_prices.0[POOL_PRICE_NUM_SAMPLES - 2],
+        HistoricalStSolPrice {
+            slot: 1388256,
+            st_sol_price_in_ust: MicroUst(909090909)
+        }
+    );
+    // For the constant product AMM:
+    // If we sell one Sol, we'll get
+    // 11*909.090909090909 - (11*(11*909.090909090909) / 12) = 833.3333333333339
+
+    assert_eq!(
+        anker.historical_st_sol_prices.0[POOL_PRICE_NUM_SAMPLES - 1],
+        HistoricalStSolPrice {
+            slot: 1820256,
+            st_sol_price_in_ust: MicroUst(833333333)
+        }
+    );
+}
+
+#[tokio::test]
 async fn test_fail_fetch_pool_price_too_early() {
     let mut context = Context::new().await;
     context
@@ -67,4 +123,10 @@ async fn test_fail_fetch_pool_price_too_early() {
         .expect("Failed to warp to slot");
     let result = context.try_fetch_pool_price().await;
     assert_solido_error!(result, AnkerError::FetchPoolPriceTooEarly);
+    context
+        .solido_context
+        .context
+        .warp_to_slot(956256 + POOL_PRICE_MIN_SAMPLE_DISTANCE + 1)
+        .expect("Failed to warp to slot");
+    context.fetch_pool_price().await;
 }
