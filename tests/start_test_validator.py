feea52629010e3676a618f73b84964970b19bb2c
E. Fynn <fynn@chorus.one>
2022-03-17 17:58:02 -0300

    Adjust tests
    - Remove test-ledger folder
    - Run python subprocess with `shell=True`

--- tests/start_test_validator.py
+++ tests/start_test_validator.py
@@ -21,6 +21,7 @@ test_validator = subprocess.Popen(
         'solana-test-validator',
     ],
     stdout=subprocess.DEVNULL,
+    shell=True,
 )
 
 # Wait up to 60 seconds for the validator to be running and processing blocks. We
