2aa01062c96532d24197e9a231a1c408c5bbfe78
Hritique Rungta <hritiquerungta@gmail.com>
2022-04-07 18:02:00 +0530

    Add option to pass address to initialize ATA

--- js/src/instructions/utils.ts
+++ js/src/instructions/utils.ts
@@ -9,24 +9,28 @@ import { PublicKey, TransactionInstruction } from '@solana/web3.js';
  * Generates the instruction to create the Associated Token Account for the given mint address
  * @param mintAddress Mint address of the token
  * @param ownerAddress Address of the owner of the token account
+ * @param associatedTokenAccountAddress Address of the (Uninitialized)Associated Token Account
  * @returns Instruction to create the Associated Token Account
  */
 export const getATAInitializeInstruction = async (
   mintAddress: PublicKey,
-  ownerAddress: PublicKey
+  ownerAddress: PublicKey,
+  associatedTokenAccountAddress?: PublicKey
 ): Promise<TransactionInstruction> => {
-  const associatedTokenAccount = await Token.getAssociatedTokenAddress(
-    ASSOCIATED_TOKEN_PROGRAM_ID,
-    TOKEN_PROGRAM_ID,
-    mintAddress,
-    ownerAddress
-  );
+  if (!associatedTokenAccountAddress) {
+    associatedTokenAccountAddress = await Token.getAssociatedTokenAddress(
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+      TOKEN_PROGRAM_ID,
+      mintAddress,
+      ownerAddress
+    );
+  }
 
   return Token.createAssociatedTokenAccountInstruction(
     ASSOCIATED_TOKEN_PROGRAM_ID,
     TOKEN_PROGRAM_ID,
     mintAddress,
-    associatedTokenAccount,
+    associatedTokenAccountAddress,
     ownerAddress,
     ownerAddress
   );
