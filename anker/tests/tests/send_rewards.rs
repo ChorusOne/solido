d9029576916304f4c675f80202d7003a1e9b03df
Ruud van Asseldonk <ruud@chorus.one>
2022-05-12 12:28:02 +0200

    Fix stack overflow in Anker::SendRewards by boxing
    Put more things on the heap to free up some stack space. This makes the
    test pass, it now fails at trying to call the program, it no longer
    overflows the stack.

--- anker/tests/tests/send_rewards.rs
+++ anker/tests/tests/send_rewards.rs
@@ -2,20 +2,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use solana_program::instruction::InstructionError;
-use std::mem;
-
-use solana_program::pubkey::Pubkey;
 use solana_program_test::tokio;
 use solana_sdk::transaction::TransactionError;
 use solana_sdk::transport::TransportError;
 
-use anker::{
-    error::AnkerError,
-    state::{POOL_PRICE_MIN_SAMPLE_DISTANCE, POOL_PRICE_NUM_SAMPLES},
-    token::MicroUst,
-};
-use lido::token::{Lamports, StLamports};
-use testlib::{anker_context::Context, assert_solido_error};
+use lido::token::Lamports;
+use testlib::anker_context::Context;
 
 const DEPOSIT_AMOUNT: u64 = 1_000_000_000; // 1e9 units
 
@@ -31,13 +23,27 @@ async fn test_send_rewards_does_not_overflow_stack() {
     let result = context.try_send_rewards().await;
 
     match result {
+        // An access violation caused by a stack overflow results in this error.
         Err(TransportError::TransactionError(TransactionError::InstructionError(
             0,
             InstructionError::ProgramFailedToComplete,
-        ))) => panic!("Did the program overflow the stack?"),
+        ))) => {
+            panic!("Did the program overflow the stack?")
+        }
+        Err(TransportError::TransactionError(TransactionError::InstructionError(
+            0,
+            InstructionError::AccountNotExecutable,
+        ))) => {
+            // This error is expected, we try to call a dummy address where the
+            // Wormhole program is supposed to live, but that dummy address is
+            // not executable. If we get here, it means we executed the entire
+            // `SendRewards` instruction aside from the final call to the
+            // Wormhole progrma. In particular we know that we didn't overflow
+            // the stack.
+        }
         Ok(()) => panic!("This should not have passed without the Wormhole program present."),
         Err(err) => {
-            println!("Unexpected error: {:?}", err);
-        } // TODO: Add a case for the expected error after resolving the stack overflow.
+            panic!("Unexpected error: {:?}", err);
+        }
     }
 }
